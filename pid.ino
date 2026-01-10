102866255f70113289c97c4542f19fa7e519e0f2
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-10 18:52:10 +0000

    Fix syntax errors in PID function

--- pid.ino
+++ pid.ino
@@ -1,12 +1,17 @@
+// Gemini PD Controller
+// A `float` function that returns the output of a PD controller
+// Inputs: Kp & Kd constants, sensor/encoder input, desired target
+// Example: float pid = PID(KP, KD, leftSensorValue, TARGET);
+
 int oldError = 0;
 
 float PID (float Kp, float Kd, float input, float target) {
   float error = target - input;
 
-  float proportional = Kp * error
-  float derivative = (oldError - error) * Kd
+  float proportional = Kp * error;
+  float derivative = (oldError - error) * Kd;
 
   oldError = error;
   
-  return proprtional + derivative
+  return proportional + derivative;
 }
