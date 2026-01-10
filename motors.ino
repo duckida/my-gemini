b88e3a1cd536bdee853586569b04f18be2ddf10f
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-10 17:57:20 +0000

    Add motors.ino

---
+++ motors.ino
@@ -0,0 +1,42 @@
+// Motor Pins
+#define M_RIGHT_FORWARD   5
+#define M_RIGHT_BACKWARD   4
+
+#define M_LEFT_FORWARD   2
+#define M_LEFT_BACKWARD   3
+
+#define SLP    7
+
+void initMotors() {
+  pinMode(M_LEFT_FORWARD, OUTPUT);
+  pinMode(M_LEFT_BACKWARD, OUTPUT);
+  pinMode(M_RIGHT_FORWARD, OUTPUT);
+  pinMode(M_RIGHT_BACKWARD, OUTPUT);
+
+  pinMode(SLP, OUTPUT);
+
+  // enable DRV8833
+  digitalWrite(SLP, HIGH);
+}
+
+void setMotors(int left, int right) {
+  int MAX_SPEED = 256;
+
+  left = MAX_SPEED - left;
+  right = MAX_SPEED - right;
+
+  // Set the forward speeds to max, and the reverse speed to max - target
+  analogWrite(M_LEFT_FORWARD, MAX_SPEED);
+  analogWrite(M_LEFT_BACKWARD, left);
+  
+  analogWrite(M_RIGHT_FORWARD, MAX_SPEED);
+  analogWrite(M_RIGHT_BACKWARD, right);
+}
+
+void stop() {
+  analogWrite(M_LEFT_FORWARD, 0);
+  analogWrite(M_LEFT_BACKWARD, 0);
+  
+  analogWrite(M_RIGHT_FORWARD, 0);
+  analogWrite(M_RIGHT_BACKWARD, 0);
+}
