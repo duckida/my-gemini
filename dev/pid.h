def884c6e8accd11d677c4e576cff5237583e563
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-18 17:12:10 +0000

    Refactor PID function into a class structure

---
+++ dev/pid.h
@@ -0,0 +1,33 @@
+// Gemini PD Controller
+// A class that returns the output of a PD controller
+// Initialize: Kp & Kd constants 
+// Calculate: sensor/encoder input, desired target
+// Example: PID motorPid(Kp, Kd)
+// motorPid.calculate(encoderDifference, 0)
+
+int oldError = 0;
+
+class PID {
+  private: 
+    float _oldError;
+    float _KP;
+    float _KD;
+
+  public:
+
+    PID(float Kp, float Kd) {
+       _KP = Kp;
+       _KD = Kd;
+      }
+      
+    float calculate(float input, float target) {
+      float error = target - input;
+
+      float proportional = _KP * error;
+      float derivative = (error - _oldError) * _KD;
+
+      _oldError = error;
+  
+      return proportional + derivative;
+    }
+};
