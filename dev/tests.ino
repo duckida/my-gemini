47bf6a8d8f54b94b18e0e7312a16e2a0153ecca1
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-18 17:12:42 +0000

    Refactor sensor and encoder PID adjustments

--- dev/tests.ino
+++ dev/tests.ino
@@ -1,16 +1,21 @@
 void testSensorPD(int target) {
-  float pid = PID(KP, KD, leftSensorValue, target);
-  setMotors(60, 60 + pid); 
+  float adj = sensorPid.calculate(leftSensorValue, target)
+  setMotors(60, 60 + adj); 
 }
 
 void testEncoderPD(int target) {
-  float pid = PID(KP, KD, encoderDifference, target);
-  setMotors(60, 60 + pid); 
+  float adj = motorPid.calculate(encoderDifference, 0);
+  setMotors(60, 60 + adj); 
 }
 
 void testDriveDistance(int distance) {
   driveDistance(60, 60, distance);
-  delay(3000);
+  delay(5000);
+}
+
+void testDriveAngle(int angle) {
+  driveAngle(60, 60, angle);
+  delay(5000);
 }
 
 void testEncoders() {
