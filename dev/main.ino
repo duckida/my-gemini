6dd6b41287ef7cacfb49c00c452c13dee01a6f3c
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-24 17:46:12 +0000

    Update main.ino

--- dev/main.ino
+++ dev/main.ino
@@ -8,6 +8,10 @@ volatile int leftSensorValue = 0;
 volatile int frontSensorValue = 0;
 volatile int rightSensorValue = 0;
 
+const int LEFT_GAP = 10;
+const int FRONT_WALL = 70;
+const int RIGHT_GAP = 10;
+
 // Encoder values
 volatile int leftEncoderValue = 0;
 volatile int rightEncoderValue = 0;
@@ -16,26 +20,34 @@ volatile int encoderDifference = 0; // right - left
 // PD controller values
 const float KP = 2;
 const float KD = 0.2;
-const int TARGET = 45;
+const int TARGET = 35;
 
+PID sensorPid(KP, KD);
 PID motorPid(KP, KD);
 
 // --- SETUP ---
 void setup() {
-  
+
   // Setup BLE Serial
   SerialBT.setName("PicoW-Gemini");
   SerialBT.begin(9600);
 
   // Initialize components
   initMotors();
+  initButtons();
+  initLeds();
 
-  delay(1000);
+  waitForButton(15);
 }
 
 // --- LOOP ---
 void loop() {
-  testDriveDistance(-100);
+  //testDriveAngle(90);
+  //testWallFollow();
+  // wallFollower();
+  //testSensorPD(TARGET);
+  printSensors();
+  testWalls();
 }
 
 // --- SETUP 1 ---
