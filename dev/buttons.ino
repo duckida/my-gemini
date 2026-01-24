08806d5422cdf0175b113100ad0b25110acc924c
Duckida <61168649+duckida@users.noreply.github.com>
2026-01-24 17:47:40 +0000

    Add button initialization and wait functions

---
+++ dev/buttons.ino
@@ -0,0 +1,12 @@
+#define LEFT_BUTTON = 15;
+#define RIGHT_BUTTON = 14;
+
+void initButtons() {
+  pinMode(LEFT_BUTTON, INPUT_PULLUP);
+  pinMode(RIGHT_BUTTON, INPUT_PULLUP);
+}
+
+void waitForButton(int pin) {
+  while (digitalRead(pin) == HIGH); 
+}
+
