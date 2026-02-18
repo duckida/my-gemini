193b21dbceddff336e42898380154c8a23f2490c
Duckida <61168649+duckida@users.noreply.github.com>
2026-02-18 17:29:05 +0000

    Refactor sensor and motion control logic

--- dev/main.ino
+++ dev/main.ino
@@ -1,5 +1,6 @@
-#include <SerialBT.h>
 #include "pid.h"
+#include "motion.h"
+#include "RPi_Pico_TimerInterrupt.h"
 
 // --- GLOBAL VARIABLES ---
 
@@ -8,9 +9,14 @@ volatile int leftSensorValue = 0;
 volatile int frontSensorValue = 0;
 volatile int rightSensorValue = 0;
 
-const int LEFT_GAP = 10;
-const int FRONT_WALL = 70;
-const int RIGHT_GAP = 10;
+int leftSensor[10];
+int frontSensor[10];
+int rightSensor[10];
+
+// Wall thresholds
+const int LEFT_GAP = 5;
+const int FRONT_WALL = 40;
+const int RIGHT_GAP = 5;
 
 // Encoder values
 volatile int leftEncoderValue = 0;
@@ -18,45 +24,66 @@ volatile int rightEncoderValue = 0;
 volatile int encoderDifference = 0; // right - left
 
 // PD controller values
-const float KP = 2;
-const float KD = 0.2;
-const int TARGET = 35;
+const float SENSOR_KP = 0.2;
+const float SENSOR_KD = 0.3;
+const int TARGET = 25;
 
-PID sensorPid(KP, KD);
-PID motorPid(KP, KD);
+PID sensorPid(SENSOR_KP, SENSOR_KD);
+PID motorPid(0.5, 0);
+
+// Timer setup
+RPI_PICO_Timer ITimer(0);
+
+// Motion controller
+Motion motion; 
+// note: 0.9 and 0.5 are good Kp/Kd value for motion
+
 
 // --- SETUP ---
 void setup() {
 
   // Setup BLE Serial
-  SerialBT.setName("PicoW-Gemini");
-  SerialBT.begin(9600);
+  Serial1.begin(57600);
 
   // Initialize components
   initMotors();
   initButtons();
   initLeds();
+  initEncoders();
 
-  waitForButton(15);
+  delay(2000);
+
+  // start timer
+  float frequency = 500.0f;
+  ITimer.attachInterrupt(frequency, TimerHandler);
+
+  // get config details
+  Serial1.println("------GEMINI------");
+
+  //int Kp = askForData("KP").toFloat();
+  //int Kd = askForData("KD").toFloat();
+  
+  motion.setup(0.9, 0.5); // 0.9, 0.5
 }
 
-// --- LOOP ---
+// -- TIMER RUNNER --
+bool TimerHandler(struct repeating_timer *t)
+{
+  motion.update();
+  return true; // Return true to keep the timer repeating
+}
+
 void loop() {
-  //testDriveAngle(90);
-  //testWallFollow();
-  // wallFollower();
-  //testSensorPD(TARGET);
-  printSensors();
-  testWalls();
-}
+  //printSensors();
+  wallFollow();
+  //testDriveAngle(-90);
+  //delay(200);
+};
 
-// --- SETUP 1 ---
 void setup1() {
   initSensors();
-  initEncoders();
 }
 
-// --- LOOP 1 = INTERUPTS ---
 void loop1() {
   updateSensors();
-}
+};
